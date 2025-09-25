e30b8f9b7468aeea29c8b4fa66b427b14ae6edff
AgnesTachyon <kuysasa007@gmail.com>
2025-09-25 13:02:10 +0700

    delete testvue

--- front/website/src/App.vue
+++ front/website/src/App.vue
@@ -1,32 +1,12 @@
 <template>
-    <div>
-        <FormComponent/>
-        <ListData :employees="employees" />
-    </div>
+  
 </template>
 
-
 <script>
-import FormComponent from "./components/FormComponent.vue"
-import ListData from "./components/ListData.vue"
 export default {
-    name: "App",
-    components:{
-        ListData,
-        FormComponent
-    },
-    data(){
-        return{
-            employees:[
-                {name:"นาส",salary:55555},
-                {name:"อาส",salary:1205},
-                {name:56,salary:"lol"} 
-            ]
-        }
-    }
+
 }
 </script>
-
 
 <style>
 
