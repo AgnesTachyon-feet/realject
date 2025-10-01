694939de6e2bea68621aca7dc95f0d22dd0b2955
AgnesTachyon <kuysasa007@gmail.com>
2025-10-01 22:51:38 +0700

    a

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 from config import engine
 
-import tables.users as user_tables
+import tables.users as users_tables
+import tables.task as task_tables
+import tables.submission as submission_tables
+import routes.task as task_routes
 import routes.users as users_routes
+import routes.submission as submission_routes
 
-user_tables.Base.metadata.create_all(bind=engine)
+
+users_tables.Base.metadata.create_all(bind=engine)
+task_tables.Base.metadata.create_all(bind=engine)
+submission_tables.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users_routes.router)
+app.include_router(task_routes.router)
+app.include_router(submission_routes.router)
 
 @app.get("/")
 async def roof():
