048508387b1e901936fb07e2830e4f4a8a1ddec9
AgnesTachyon <kuysasa007@gmail.com>
2025-10-01 22:52:36 +0700

    a

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
 from config import engine
 
-import tables.users as users_tables
+import tables.users as user_tables
 import tables.task as task_tables
 import tables.submission as submission_tables
 import routes.task as task_routes
-import routes.users as users_routes
+import routes.users as user_routes
 import routes.submission as submission_routes
 
 
-users_tables.Base.metadata.create_all(bind=engine)
+user_tables.Base.metadata.create_all(bind=engine)
 task_tables.Base.metadata.create_all(bind=engine)
 submission_tables.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-app.include_router(users_routes.router)
+app.include_router(user_routes.router)
 app.include_router(task_routes.router)
 app.include_router(submission_routes.router)
 
